3889429fb97c480d686d99fd8690962914bcb93c
NicolasDona <nicolas.donascimento@gmail.com>
2023-12-06 19:08:42 +0100

    wordpress update

---
+++ wp-content/themes/wpuppies/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- style CSS perso -->
+    <link rel="stylesheet" href="/wp-content/themes/wpuppies/style.css">
+    <title>Puppies Theme</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img class="tlogo" src="/wp-content/themes/wpuppies/logo.png"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <ul class="navbar-nav ms-auto"><!--Ajustement Droite-->
+                    <li class="nav-item me-5">
+                        <a class="text-decoration-none hover-underline" href="#">Catégorie 1&nbsp;</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="text-decoration-none hover-underline" href="#">Catégorie 2&nbsp;</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="text-decoration-none hover-underline" href="#">Catégorie 3&nbsp;</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="bg-image img-fluid text-center text-white font-title title-ps">
+                        <h1>Titre du site</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </header>
+    <main>
+    </main>
+
+    <footer class="footer-st">
+    <div class="font-title text-center p-5">
+    <h4> DECOUVRER</h4>
+    <h3>Articles les plus populaires</h3>
+    </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-4">
+                    <div class="card mt-5">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card mt-5">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card mt-5">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Block Gauche -->
+        <div class="container p-5 ">
+            <div class="row letter-sp">
+                <div class="col-5">
+                    <h5 class="font-title fw-bold">Accès rapide</h5>
+                    <p><a href="#" target="_blank">Accueil</i></a></p>
+                    <p><a href="#" target="_blank">Catégorie 1</i></a></p>
+                    <p><a href="#" target="_blank">Catégorie 2</i></a></p>
+                    <p><a href="#" target="_blank">Catégorie 3</i></a></p>
+                </div>
+                <!-- Partie centrale -->
+                <div class="col-4">
+                    <h5 class="font-title fw-bold">Contactez-nous</h5>
+                    <p></p>
+                    <ul class="list-unstyled mb-0">
+                        <li class="mb-1 regular">
+                            <p>70 rue des jacobins<br>80000 Amiens</p>
+                        </li>
+                        <li>
+                            <a href="mailto:contactus@lamanu.fr" title="Lien de l'email" class="regular text-decoration-none hover-underline fw-bold">contactus@lamanu.fr</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Partie Droite  -->
+                <div class="col-3">
+                    <h5 class="mb-1 letter-sp font-title fw-bold">Inscrivez-vous à notre newsletter</h5>
+                    <table class="regular letter-sp">
+                        <tbody>
+                            <tr>
+                                <td><input type="email"></input></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <script src="/wp-content/themes/wpuppies/script.js"></script>
+    </footer>
+
+</body>
+
+</html>
