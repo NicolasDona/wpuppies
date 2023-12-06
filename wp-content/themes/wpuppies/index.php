66f23ffe10a4eca86388c300a7bcd0a8f1f843b5
NicolasDona <nicolas.donascimento@gmail.com>
2023-12-06 22:31:39 +0100

    centrage marges colors

--- wp-content/themes/wpuppies/index.php
+++ wp-content/themes/wpuppies/index.php
@@ -22,13 +22,13 @@
                 </button>
                 <ul class="navbar-nav ms-auto"><!--Ajustement Droite-->
                     <li class="nav-item me-5">
-                        <a class="text-decoration-none hover-underline" href="#">Catégorie 1&nbsp;</a>
+                        <a class="text-decoration-none hover-underline" href="#">Catégorie 1</a>
                     </li>
                     <li class="nav-item me-5">
-                        <a class="text-decoration-none hover-underline" href="#">Catégorie 2&nbsp;</a>
+                        <a class="text-decoration-none hover-underline" href="#">Catégorie 2</a>
                     </li>
                     <li class="nav-item me-5">
-                        <a class="text-decoration-none hover-underline" href="#">Catégorie 3&nbsp;</a>
+                        <a class="text-decoration-none hover-underline" href="#">Catégorie 3</a>
                     </li>
                 </ul>
             </div>
@@ -36,7 +36,7 @@
         </nav>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-12 title-center">
                     <div class="bg-image img-fluid text-center text-white font-title title-ps">
                         <h1>Titre du site</h1>
                     </div>
@@ -49,83 +49,68 @@
     </main>
 
     <footer class="footer-st">
-    <div class="font-title text-center p-5">
-    <h4> DECOUVRER</h4>
-    <h3>Articles les plus populaires</h3>
-    </div>
+        <div class="font-title text-center p-5">
+            <p class="font-title fs-3 text-white">DECOUVREZ</p>
+            <p class="font-title fs-2 text-white">Articles les plus populaires</P>
+        </div>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-4">
                     <div class="card mt-5">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <img class="card-img-top" src="/wp-content/themes/wpuppies/bg.jpg" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <span class="badge color-card-badge">Catégorie</span>
+                            <h5 class="card-title color-card-title">Titre de l'article</h5>
+                            <p class="card-text color-card-text">Description de l'article sur plusieurs lignes.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="card mt-5">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <img class="card-img-top img-fluid" src="/wp-content/themes/wpuppies/bg2.jpg" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <span class="badge color-card-badge">Catégorie</span>
+                            <h5 class="card-title color-card-title">Titre de l'article</h5>
+                            <p class="card-text color-card-text">Description de l'article sur plusieurs lignes.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="card mt-5">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <img class="card-img-top" src="/wp-content/themes/wpuppies/bg.jpg" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <span class="badge color-card-badge">Catégorie</span>
+                            <h5 class="card-title color-card-title">Titre de l'article</h5>
+                            <p class="card-text color-card-text">Description de l'article sur plusieurs lignes.</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Block Gauche -->
-        <div class="container p-5 ">
+        <div class="container-fluid justify-content-center text-center p-5 ">
             <div class="row letter-sp">
-                <div class="col-5">
-                    <h5 class="font-title fw-bold">Accès rapide</h5>
-                    <p><a href="#" target="_blank">Accueil</i></a></p>
-                    <p><a href="#" target="_blank">Catégorie 1</i></a></p>
-                    <p><a href="#" target="_blank">Catégorie 2</i></a></p>
-                    <p><a href="#" target="_blank">Catégorie 3</i></a></p>
+                <!-- Block Gauche -->
+                <div class="col-4 text-center">
+                    <h5 class="font-title fw-bold text-center text-white">Accès rapide</h5>
+                    <p><a href="#" class="regular text-decoration-none hover-underline" target="_blank">Accueil</a></p>
+                    <p><a href="#" class="regular text-decoration-none hover-underline" target="_blank">Catégorie 1</a></p>
+                    <p><a href="#" class="regular text-decoration-none hover-underline" target="_blank">Catégorie 2</a></p>
+                    <p><a href="#" class="regular text-decoration-none hover-underline" target="_blank">Catégorie 3</a></p>
                 </div>
                 <!-- Partie centrale -->
-                <div class="col-4">
-                    <h5 class="font-title fw-bold">Contactez-nous</h5>
-                    <p></p>
-                    <ul class="list-unstyled mb-0">
-                        <li class="mb-1 regular">
-                            <p>70 rue des jacobins<br>80000 Amiens</p>
-                        </li>
-                        <li>
-                            <a href="mailto:contactus@lamanu.fr" title="Lien de l'email" class="regular text-decoration-none hover-underline fw-bold">contactus@lamanu.fr</a>
-                        </li>
-                    </ul>
+                <div class="col-4 text-center">
+                    <h5 class="font-title fw-bold text-white">Contactez-nous</h5>
+                    <p class="regular">70 rue des jacobins<br>80000 Amiens</p>
+                    <p><a href="mailto:contactus@lamanu.fr" title="Lien de l'email" class="regular text-decoration-none hover-underline fw-bold">contactus@lamanu.fr</a></p>
                 </div>
                 <!-- Partie Droite  -->
-                <div class="col-3">
+                <div class="col-4 text-center">
                     <h5 class="mb-1 letter-sp font-title fw-bold">Inscrivez-vous à notre newsletter</h5>
-                    <table class="regular letter-sp">
-                        <tbody>
-                            <tr>
-                                <td><input type="email"></input></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <input type="email" class="" placeholder="Entrez votre email"></input>
                 </div>
             </div>
         </div>
         <script src="/wp-content/themes/wpuppies/script.js"></script>
     </footer>
-
 </body>
-
 </html>
